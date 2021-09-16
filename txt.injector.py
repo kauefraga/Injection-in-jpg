d9e1b05a9f3da505cb8159497ae7fbc094b84d1d
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2021-09-16 11:53:44 -0300

    re-writing

--- txt.injector.py
+++ txt.injector.py
@@ -1,3 +1,3 @@
 # text injection
-with open('OtherFile.jpg', 'ab') as a: # put your image in 'assets' and change the path
+with open('OtherImage.jpg', 'ab') as a: # put your image in 'assets' and change the path
   a.write(b"YourString") # your text inside of string
