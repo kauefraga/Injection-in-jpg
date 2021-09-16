ef0c28193deb3def91e6b884dfa4d4e1021a3e60
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2021-09-16 11:52:25 -0300

    re-writing

--- png_injector.py
+++ png_injector.py
@@ -1,10 +1,10 @@
 import PIL.Image
 import io
 
-img = PIL.Image.open('./assets/TheWoman.png')
+img = PIL.Image.open('YourImage.png')
 byte_arr = io.BytesIO()
 img.save(byte_arr, format='PNG')
 
 # png injection
-with open('./assets/TheMan.jpg', 'ab') as c:
+with open('OtherImage.jpg', 'ab') as c:
   c.write(byte_arr.getvalue())
