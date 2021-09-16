8b63a67392a3534bd0b9c58ef08d37c92179627a
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2021-09-16 11:45:28 -0300

    Delete injectInJPG directory

--- injectInJPG/txt.injector.py
+++
@@ -1,3 +0,0 @@
-# text injection
-with open('./assets/TheMan.jpg', 'ab') as a: # put your image in 'assets' and change the path
-  a.write(b" ---- ") # your text inside of string
--- injectInJPG/txt_founder.py
+++
@@ -1,7 +0,0 @@
-# text founder
-with open('./assets/TheMan.jpg', 'rb') as b:
-  content = b.read()
-  offset = content.index(bytes.fromhex('FFD9'))
-
-  b.seek(offset + 2)
-  print(b.read())
